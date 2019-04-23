1fcecfe2ca8cd8bb2afc5018bc1d5ad419b8771a
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2019-04-23 06:59:15 -0300

    add fn clear
    restored index.html for the original

--- js/designs.js
+++ js/designs.js
@@ -6,42 +6,51 @@
 const pixel = {
   // initialization
   init: function () {
-    document.getElementById('sizePicker').addEventListener('submit', pixel.draw_board, false);
+    document.getElementById('sizePicker').addEventListener('submit', pixel.draw, false);
   },
   // building grid
-  draw_board: function makeGrid(e) {
+  draw: function makeGrid(e) { 
     e.preventDefault();
     const table = document.querySelector('#pixelCanvas');
     const rows = document.querySelector('#inputHeight').value;
     const cells = document.querySelector('#inputWidth').value;
+    const button = document.getElementById('sizePicker');
 
     if (rows > 30 || cells > 40) {
       window.alert(`Attention\nThe maximum value for the Grid Height is 40 and for Grid Width is 60.\nPlease check the reported values.`);
     } else {
-      for (let i = 0; i <= rows; i++) {
+      //rows
+      for (let i = 1; i <= rows; i++) {
         let rowsHtml = document.createElement('tr');
-
-        for (let i = 0; i <= cells; i++) {
+        //cells
+        for (let i = 1; i <= cells; i++) {
           cellsHtml = document.createElement('td');
-
+          //add fn in click
           cellsHtml.addEventListener('click', function () {
-            pixel.to_paint(this);
+            pixel.paint(this);
           })
-
           rowsHtml.appendChild(cellsHtml);
         }
-
         table.appendChild(rowsHtml);
       }
+      //new fn in submit
+      button.addEventListener('click', pixel.clear);
     }
   },
   // get color pick
-  pick_color: function () {
+  color: function () {
     return document.getElementById('colorPicker').value;
   },
   // painting cell
-  to_paint: function (e) { 
-    e.style.backgroundColor = pixel.pick_color();
+  paint: function (e) {
+    e.style.backgroundColor = pixel.color();
+  },
+  // clear grid
+  clear: function () { 
+    const rows = document.querySelectorAll('#pixelCanvas tr');
+    rows.forEach(function (row) {
+      row.remove();
+    })
   }
 }
 
